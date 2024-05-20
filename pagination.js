0d4ae4b45b2955fbb9c8754162f8749201b60b6b
ekke-moor <155710381+ekke-moor@users.noreply.github.com>
2024-05-20 08:57:23 +0300

    Update pagination.js

--- pagination.js
+++ pagination.js
@@ -1,24 +1,21 @@
-
 const data = [
   { sn: "00002356517", name:'Valuveljed' },
-    { sn: "00002356518", name:'BMV handbook' },
-    { sn: "00002356519", name:'Honda handbook' }
+  { sn: "00002356518", name:'BMV handbook' },
+  { sn: "00002356519", name:'Honda handbook' }
 ];
 
 const pageSize = 2; // Number of items per page
 
-app.get('/api/users', (req, res) => {
+app.get('/api/spare-parts', (req, res) => {
   const pageNumber = req.query.page || 1; // Get the current page number from the query parameters
   const startIndex = (pageNumber - 1) * pageSize;
   const endIndex = startIndex + pageSize;
-  const users = data.slice(startIndex, endIndex);
+  const spare-parts = data.slice(startIndex, endIndex);
 
   res.json({
-    users,
+    spare-parts,
     total: data.length,
     totalPages: Math.ceil(data.length / pageSize),
     currentPage: pageNumber
   });
 });
-
-```
